2259c1cdedbad4b5076d4c23a843406e246f21d0
Natalia Barbosa <natalia.barbosa@zig.fun>
2023-12-23 15:06:54 -0300

    feat: app wreapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Header } from "./components/Header/Header"
+import s from './App.module.scss';
+import { SideBar } from "./components/SideBar/SideBar";
 
 function App() {
 
   return (
     <div>
       <Header />
+      <div className={s.wrapper}>
+        <SideBar />
+      </div>
+
     </div>
   )
 }
